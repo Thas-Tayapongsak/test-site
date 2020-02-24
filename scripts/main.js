6c074d5319b0e15bd2e6060dedf217db155d2c7a
Thas-Tayapongsak <hwunhwun@gmail.com>
2020-02-24 13:20:38 +0700

    added interactivity

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,13 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+let myImage = document.querySelector('img');
+let selector = document.querySelector('#selector');
+
+selector.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/cat.jpg') {
+      myImage.setAttribute ('src','images/cat2.jpg');
+      selector.textContent = 'click to change to my cat';
+    } else {
+      myImage.setAttribute ('src','images/cat.jpg');
+      selector.textContent = 'click to change to my friend\'s cat';
+    }
+}
